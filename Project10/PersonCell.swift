7f8c3a743a1727a715b436ce8faacdc6286b13af
Maksim Li <maximlee.ml@gmail.com>
2024-12-07 16:21:08 +0200

    Initial Commit

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Maksim Li on 07/12/2024.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var name: UILabel!
+    
+}
